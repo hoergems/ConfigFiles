6ca7d889d99bd89f227c182d7e327a812e01725f
hoergems <hoergems@gmail.com>
2017-03-27 15:39:22 +1000

    Added 4DOF config script

--- WAFRJournal/randomScene/Dubin/collisionsNotAllowed/20_obstacles/gen.py
+++ WAFRJournal/randomScene/Dubin/collisionsNotAllowed/20_obstacles/gen.py
@@ -2,16 +2,33 @@ import sys
 import os
 import subprocess
 import glob
+import argparse
 
-covarianceStepSize = (0.01 - 0.0001) / 9.0
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('-nO', '--numObstacles', type=int, default=5,
+                    help='Number of obstacles')
+parser.add_argument('-r', '--robot', type=str, default="dubin", help="The robot")
+args = parser.parse_args()
+
+
+numObstacles = args.numObstacles
+robot = args.robot
+robotExecName = "dubin"
+environmentTemplate = "maze_environment_random_"
+maxCovariance = 0.01
+if robot == "4DOF":
+    robotExecName = "robot"
+    environmentTemplate = "world_random_RobotImpl_"
+    maxCovariance = 100.0
+    
+covarianceStepSize = (maxCovariance - 0.0001) / 9.0
 covariance = 0.0001
-numObstacles = 20
 
 if not os.path.isdir("cfg"):
 	os.makedirs("cfg")	
 
 for i in xrange(1, 11):
-    with open("dubin.cfg", 'r') as f:
+    with open(robot + ".cfg", 'r') as f:
 	data = f.readlines()
 	for j in xrange(len(data)):
 	    if "minProcessCovariance" in data[j]:
@@ -19,16 +36,16 @@ for i in xrange(1, 11):
 	    elif "minObservationCovariance" in data[j]:
 		data[j] = "minObservationCovariance = " + str(covariance) + " \n"
             elif "logPath" in data[j]:
-		data[j] = "logPath = /datastore/hoe01h/WAFRJournal/randomScene/Dubin/collisionsNotAllowed/" + str(numObstacles) + "_obstacles/" + str(i) + " \n"
+		data[j] = "logPath = /datastore/hoe01h/WAFRJournal/randomScene/" + robot + "/collisionsNotAllowed/" + str(numObstacles) + "_obstacles/" + str(i) + " \n"
 	if not os.path.isdir("cfg/" + str(i)):
 	    os.makedirs("cfg/" + str(i))	    
 	for j in xrange(100):
 	    for k in xrange(len(data)):
 		if "planningEnvironmentPath" in data[k]:		    
-		    data[k] = "planningEnvironmentPath = maze_environment_random_" + str(numObstacles) + "_" + str(j) + ".sdf \n"
+		    data[k] = "planningEnvironmentPath = " + environmentTemplate + str(numObstacles) + "_" + str(j) + ".sdf \n"
 		if "executionEnvironmentPath" in data[k]:
-		    data[k] = "executionEnvironmentPath = maze_environment_random_" + str(numObstacles) + "_" + str(j) + ".sdf \n"
-            with open("cfg/" + str(i) + "/dubin" + str(j) + ".cfg", 'a+') as l:
+		    data[k] = "executionEnvironmentPath = " + environmentTemplate + str(numObstacles) + "_" + str(j) + ".sdf \n"
+            with open("cfg/" + str(i) + "/" + robotExecName + str(j) + ".cfg", 'a+') as l:
 		for k in xrange(len(data)):
 		    l.write(data[k])	
 	
