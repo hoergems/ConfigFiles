1a9fa2a1f872f420b84f4617cd44d5fa4ed3d2f5
hoergems <hoergems@gmail.com>
2017-11-29 15:46:21 +1000

    f

--- WAFRJournal/SNMvsMONG/4DOFManipulator/4DOFEmptyEnvironment/genTrajectorySamples/gen.py
+++ WAFRJournal/SNMvsMONG/4DOFManipulator/4DOFEmptyEnvironment/genTrajectorySamples/gen.py
@@ -26,8 +26,6 @@ environmentTemplate = args.environmentTemplate
 resultsPath = args.resultsPath
 if (resultsPath.strip()[-1] != "/"):
     resultsPath += "/"
-if not os.path.isdir(resultsPath):    
-    os.makedirs(resultsPath)
  
 minCovariance = float(args.minCovariance)
 maxCovariance = float(args.maxCovariance)
@@ -60,8 +58,8 @@ for i in xrange(1, covarianceSteps+1):
                     elif "observationError" in data[k]:
                         data[k] = "observationError = " + str(observationCovariance) + " \n"
                     elif "logPath" in data[k]:
-                        dr = resultsPath + environmentTemplate + "/" + str(i) + "_proc_" + str(j) + "_obs/"
-                        data[k] = "logPath = " + resultsPath + environmentTemplate + "/" + str(numObstacles) + "_obstacles/" + str(i) + "_proc_" + str(j) + "_obs/ \n"
+                        dr = resultsPath + environmentTemplate + "/" + str(numObstacles) + "_obstacles/" + str(i) + "_proc_" + str(j) + "_obs/"
+                        data[k] = "logPath = " + dr + " \n"
                         if not os.path.exists(dr):
                             os.makedirs(dr)
                     elif "logFilePostfix" in data[k]:
