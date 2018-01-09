dcedbfe4f74300278aea116e7f53d7bf0fb9de4b
hoergems <hoergems@gmail.com>
2018-01-09 15:59:55 +1000

    Up

--- WAFRJournal/SNM/DubinMaze/gen.py
+++ WAFRJournal/SNM/DubinMaze/gen.py
@@ -13,6 +13,7 @@ parser.add_argument('-maxc', '--maxCovariance', type=float, default=0.01, help="
 parser.add_argument('-cs', '--covarianceSteps', type=int, default=10, help="Number of covariance steps")
 parser.add_argument('-nr', '--numRuns', type=int, default=100, help="Number of runs")
 parser.add_argument('-ct', '--environmentTemplate', type=str, default="4DOFFactory1", help="The config file to process")
+parser.add_argument('-pf', '--postFix', type=str, default="", help="Log file postfix")
 parser.add_argument('-rp', '--resultsPath', type=str, default="/datastore/hoe01h/WAFRJournal", help="The path where the log files are stored in")
 args = parser.parse_args()
 
@@ -25,6 +26,7 @@ resultsPath = args.resultsPath
 if (resultsPath.strip()[-1] != "/"):
     resultsPath += "/"
  
+postFix = args.postFix
 minCovariance = float(args.minCovariance)
 maxCovariance = float(args.maxCovariance)
 covarianceSteps = int(args.covarianceSteps)
@@ -42,33 +44,33 @@ if os.path.isdir(folder):
 os.makedirs(folder)
 
 for i in xrange(1, covarianceSteps+1):
-	observationCovariance = minCovariance
-	j=i
-	folder = "cfg" + "/" + str(i) + "_proc_" + str(j) + "_obs"
-	os.makedirs(folder)
-	for l in xrange(numRuns) :
-	    with open(environmentTemplate + ".cfg", 'r') as f:
-			data = f.readlines()
-			for k in xrange(len(data)):
-				if "processError" in data[k]:
-					data[k] = "processError = " + str(processCovariance) + " \n"
-				elif "observationError" in data[k]:
-					data[k] = "observationError = " + str(processCovariance) + " \n"
-				elif "logPath" in data[k]:
-					dr = resultsPath + environmentTemplate + "/" + str(i) + "_proc_" + str(j) + "_obs/"
-					data[k] = "logPath = " + dr + " \n"
-					if not os.path.exists(dr):
-						os.makedirs(dr)
-				elif "logFilePostfix" in data[k]:
-					data[k] = "logFilePostfix = " + str(l) + " \n"	
-				elif "outputFile" in data[k] and not "measureOutputFile" in data[k]:
-					data[k] = "outputFile = /data/hoe01h/oppt_devel/files/trajectorySamplesDubin/trajectorySamplesDubinMaze.txt \n"
-				elif "measureOutputFileEMD" in data[k]:
-					data[k] = "measureOutputFileEMD = /data/hoe01h/oppt_devel/files/measureSamplesDubin/measureSamplesDubinMazeEMD" + str(i) + ".txt \n" 
-				elif "measureOutputFileTV" in data[k]:
-					data[k] = "measureOutputFileTV = /data/hoe01h/oppt_devel/files/measureSamplesDubin/measureSamplesDubinMazeTV" + str(i) + ".txt \n" 
-			with open(folder + "/" + environmentTemplate + "_" + str(l) + ".cfg", 'a+') as l:		
-				for k in xrange(len(data)):
-					l.write(data[k])		
-	observationCovariance += covarianceStepSize
-	processCovariance += covarianceStepSize
+    observationCovariance = minCovariance
+    j=i
+    folder = "cfg" + "/" + str(i) + "_proc_" + str(j) + "_obs"
+    os.makedirs(folder)
+    for l in xrange(numRuns) :
+	with open(environmentTemplate + ".cfg", 'r') as f:
+	    data = f.readlines()
+	    for k in xrange(len(data)):
+		if "processError" in data[k]:
+		    data[k] = "processError = " + str(processCovariance) + " \n"
+		elif "observationError" in data[k]:
+		    data[k] = "observationError = " + str(processCovariance) + " \n"
+		elif "logPath" in data[k]:
+		    dr = resultsPath + environmentTemplate + "/" + str(i) + "_proc_" + str(j) + "_obs/"
+		    data[k] = "logPath = " + dr + " \n"
+		    if not os.path.exists(dr):
+			os.makedirs(dr)
+		elif "logFilePostfix" in data[k]:
+		    data[k] = "logFilePostfix = " + postfix + str(l) + " \n"	
+		elif "outputFile" in data[k] and not "measureOutputFile" in data[k]:
+		    data[k] = "outputFile = /data/hoe01h/oppt_devel/files/trajectorySamplesDubin/trajectorySamplesDubinMaze.txt \n"
+		elif "measureOutputFileEMD" in data[k]:
+		    data[k] = "measureOutputFileEMD = /data/hoe01h/oppt_devel/files/measureSamplesDubin/measureSamplesDubinMazeEMD" + str(i) + ".txt \n" 
+		elif "measureOutputFileTV" in data[k]:
+		    data[k] = "measureOutputFileTV = /data/hoe01h/oppt_devel/files/measureSamplesDubin/measureSamplesDubinMazeTV" + str(i) + ".txt \n" 
+	    with open(folder + "/" + environmentTemplate + "_" + str(l) + ".cfg", 'a+') as l:		
+		for k in xrange(len(data)):
+		    l.write(data[k])		
+    observationCovariance += covarianceStepSize
+    processCovariance += covarianceStepSize
