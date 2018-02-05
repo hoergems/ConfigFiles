91127a4a7943d9ec4d63d4b91b8d0ee508996274
hoergems <hoergems@gmail.com>
2018-02-05 11:52:25 +1000

    Add

---
+++ approximateDynamics/KukaTable/gen.py
@@ -0,0 +1,20 @@
+import os
+
+
+for i in xrange(1000):
+    outFile = "KukaTable" + str(i) + ".cfg"
+    if os.path.isfile(outFile):
+	os.remove(outFile)
+    with open("KukaTable.cfg", 'r') as f:
+	with open(outFile, 'a+') as of:	    
+	    for line in f.readlines():
+		if "logFilePostfix" in line:
+		    of.write("logFilePostfix = " + str(i) + "\n")
+		elif "outputFile =" in line:
+		    of.write("outputFile = /data/hoe01h/oppt_devel/files/approximateDynamics/KukaTable/trajectorySamplesKukaTable" + str(i) + ".txt \n")
+		elif "heuristicOutputFile = " in line:
+		    of.write("heuristicOutputFile = /data/hoe01h/oppt_devel/files/approximateDynamics/KukaTable/heuristicSamplesKukaTable" + str(i) + ".txt \n")
+		elif "numThreads =" in line:
+		    of.write("numThreads = 6 \n")
+		else:		    
+		    of.write(line)   
